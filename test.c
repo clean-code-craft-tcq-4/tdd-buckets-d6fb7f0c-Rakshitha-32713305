0035045daac77efe0d5950a573bd91dbb6389f7a
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-08-01 16:34:12 +0530

    Update test.c

--- test.c
+++ test.c
@@ -5,10 +5,10 @@ int main()
 {
 int *chkrange;
 
-int samplesarray[] = {3, 3, 5, 4, 10, 11, 12};
-int arraysize = sizeof(samplesarray) / sizeof(samplesarray[0]);
+int array[] = {3, 3, 5, 4, 10, 11, 12};
+int arraysize = sizeof(array) / sizeof(array[0]);
   
-chkrange = detect_PrintRangeDetails(samplesarray,arraysize);
+chkrange = detect_PrintRange(array,arraysize);
 
 assert(*(chkrange+4) == 3);
 assert(*(chkrange+5) == 5);
