3ba900744b2c79ac9eb099ade9fedd376d4d749d
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-08-01 16:50:48 +0530

    Update CurrentRange.h

--- CurrentRange.h
+++ CurrentRange.h
@@ -1,10 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct Range 
-{
-   int startvalue; int endvalue; int rangecount;
-}Range;
+int * PrintRange(int array[],int arraysize);
 
-int * PrintRange(int currentsamplesarray[],int arraysize);
-int isConsecutive(int diff);
