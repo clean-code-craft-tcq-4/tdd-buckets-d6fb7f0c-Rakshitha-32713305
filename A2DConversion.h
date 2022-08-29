0fc6c40f56abd46e326f98617890eba52173a9a7
Rakshitha-32713305 <105207364+Rakshitha-32713305@users.noreply.github.com>
2022-08-29 14:19:46 +0530

    Create A2DConversion.h

---
+++ A2DConversion.h
@@ -0,0 +1,2 @@
+int convertA2DToAmpere(int maxAmp,int A2Dvalue,int bitresolution);
+void convertA2DToAmpereRange(int A2Dvaluearray[], int arraysize, int* Amperearray, int maxAmp, int bitresolution);
